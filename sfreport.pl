94b8679791be9c59d62eea492cc3b29eb99efb1e
amitgu-xilinx <gupta.amit@amd.com>
2023-11-24 12:16:13 +0000

    Update sfreport.pl Copyright order

--- sfreport.pl
+++ sfreport.pl
@@ -1,10 +1,11 @@
 #!/usr/bin/perl -w
 # SPDX-License-Identifier: GPL-2.0
-# Reporting tool for AMD Solarflare under linux
+# Copyright (C) 2022-2023, Advanced Micro Devices, Inc.
 # Copyright (C) 2007-2019, Solarflare Communications.
 # Copyright (C) 2019-2022, Xilinx, Inc.
-# Copyright (C) 2022-2023, Advanced Micro Devices, Inc.
-#
+
+# Reporting tool for AMD Solarflare under linux
+
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 2 as published
 # by the Free Software Foundation, incorporated herein by reference.
